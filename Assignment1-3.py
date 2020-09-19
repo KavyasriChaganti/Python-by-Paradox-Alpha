54ab51e1aef2ede32b0a0ad3d7575c65e62ddaca
KavyasriChaganti <53474036+KavyasriChaganti@users.noreply.github.com>
2020-09-19 19:31:56 +0530

    Create Assignment1-3.py

---
+++ Assignment1-3.py
@@ -0,0 +1,12 @@
+3.Write a program to declare two variable , take one of them from 
+user and determine one by self then compare and print them.
+Program: 
+        var1="kavya"
+        var2=input("enter any name : ")
+        print(var2)
+        print(var2==var1)
+        
+Output: enter any name : kavya
+        kavya
+        True
+        
