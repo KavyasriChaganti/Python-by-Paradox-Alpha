d87f680caa277301eea9b5134ab08961b915138c
KavyasriChaganti <53474036+KavyasriChaganti@users.noreply.github.com>
2020-09-19 19:19:37 +0530

    Create Assignment1-2.py

---
+++ Assignment1-2.py
@@ -0,0 +1,34 @@
+Define Identifiers And Datatypes?
+IDENTIFIERS :
+
+An identifier is a name given to entities like class, functions, 
+variables, etc. It helps to differentiate one entity from another.
+Rules for identifiers:
+* Identifiers can be a combination of letters in lowercase (a to z) 
+or uppercase (A to Z) or digits (0 to 9) or an underscore _. Names 
+like myClass, var_1 and print_this_to_screen, all are valid example.
+*  An identifier cannot start with a digit. 1variable is invalid, 
+but variable1 is perfectly fine. 
+* Keywords cannot be used as identifiers.
+* We cannot use special symbols like !, @, #, $, % etc. in our identifier.
+* Identifier can be of any length.
+
+
+DATATYPES : 
+
+Data types are the classification or categorization of data items. Data types 
+represent a kind of value which determines what operations can be performed on 
+that data. Numeric, non-numeric and Boolean (true/false) data are the most used 
+data types. However, each programming language has its own classification largely 
+reflecting its programming philosophy.
+* Int #we use it for number
+* Char # we use it for only single alphabet
+* Str #we use it for name and any collect of alphabet
+* Float #we use it for number in decimal
+* Complex #we use it for number in ai+j form
+* Double #we use it for number and char Inc. the size 
+* Long #we use it for number and char also it creates whole block and also Inc. the size
+* Short #we use it for number
+* Unsigned #we use it for number
+* Signed, etc #we use it for number.
+
